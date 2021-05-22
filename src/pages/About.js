d6aaaf61ea67dad028dc3ae32747cb0e1b7f4073
Kasireddi Himabindu <himabindu.kasireddi@adypu.edu.in>
2021-05-22 22:04:02 +0530

    Final changes done

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Me</h1>
+      <p>
+        Hey, I am Hiren Chafekar! I love to build web-apps and I am in love with React Js. 
+        Why don't you learn it too? It will be fun. Together we can have a toast to learning. You can choose any of the cocktails.
+      </p>
+    </section>
+  );
+}
